b88a05727241d1b9807236edc2e14a291acddbb2
Shani Bhingaradiya <shanibhingaradiya@gmail.com>
2022-01-27 12:55:53 +0530

    test

--- lib/screens/startup/startup_home_screen.dart
+++ lib/screens/startup/startup_home_screen.dart
@@ -40,115 +40,82 @@ class _StartupHomeScreenState extends State<StartupHomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-            title: OnBoardingAppBarTitle(),
-            centerTitle: true,
-            leading: Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: InkWell(
-                  onTap: () {
-                    FirebaseAuth.instance.signOut();
-                    Get.off(StartScreen());
-                    removeSharedPreferences();
-                  },
-                  child: CircleAvatar(
-                    radius: 25,
-                    backgroundImage: AssetImage("assets/test_image.png"),
-                  )),
-            ),
-            backgroundColor: Colors.white,
-            actions: [
-              Padding(
-                  padding: const EdgeInsets.fromLTRB(0, 0, 10, 0),
-                  child: IconButton(
-                    onPressed: () {},
-                    icon: ImageIcon(
-                      AssetImage("assets/chat_icon.png"),
-                      color: Theme.of(context).primaryColor,
-                    ),
-                  )),
-            ],
-            bottom: Get.find<StartupGlobalController>().currentIndex.value == 1
-                ? TabBar(
-                    tabs: [
-                      Tab(
-                        child: Text(
-                          "Sent",
-                          style: TextStyle(
-                            color: Theme.of(context).primaryColor,
-                            fontWeight: FontWeight.w700,
-                            fontSize: 20,
-                            fontFamily: 'Cabin',
-                            letterSpacing: 1,
-                          ),
-                        ),
-                      ),
-                      Tab(
-                        child: Text(
-                          "Received",
-                          style: TextStyle(
-                            color: Theme.of(context).primaryColor,
-                            fontWeight: FontWeight.w700,
-                            fontSize: 20,
-                            fontFamily: 'Cabin',
-                            letterSpacing: 1,
-                          ),
-                        ),
-                      ),
-                    ],
-                  )
-                : TabBar(tabs: [],)),
-        body: Obx(
-          () => startupGlobalController.isLoading.value
-              ? Center(
-                  child: CircularProgressIndicator(),
-                )
-              : IndexedStack(
-                  index: startupGlobalController.currentIndex.value,
-                  children: screens,
-                ),
+    return Scaffold(
+      appBar: AppBar(
+        title: OnBoardingAppBarTitle(),
+        centerTitle: true,
+        leading: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: InkWell(
+              onTap: () {
+                FirebaseAuth.instance.signOut();
+                Get.off(StartScreen());
+                removeSharedPreferences();
+              },
+              child: CircleAvatar(
+                radius: 25,
+                backgroundImage: AssetImage("assets/test_image.png"),
+              )),
         ),
-        bottomNavigationBar: Obx(
-          () => BottomNavigationBar(
-            currentIndex:
-                Get.find<StartupGlobalController>().currentIndex.value,
-            onTap: (index) {
-              Get.find<StartupGlobalController>().currentIndex.value = index;
-            },
-            items: [
-              BottomNavigationBarItem(
+        backgroundColor: Colors.white,
+        actions: [
+          Padding(
+              padding: const EdgeInsets.fromLTRB(0, 0, 10, 0),
+              child: IconButton(
+                onPressed: () {},
                 icon: ImageIcon(
-                  AssetImage("assets/investor_detail_icon.png"),
+                  AssetImage("assets/chat_icon.png"),
+                  color: Theme.of(context).primaryColor,
                 ),
-                backgroundColor: Theme.of(context).primaryColor,
-                label: 'Investors',
+              )),
+        ],
+      ),
+      body: Obx(
+        () => startupGlobalController.isLoading.value
+            ? Center(
+                child: CircularProgressIndicator(),
+              )
+            : IndexedStack(
+                index: startupGlobalController.currentIndex.value,
+                children: screens,
               ),
-              BottomNavigationBarItem(
-                icon: ImageIcon(
-                  AssetImage("assets/connection_request_icon.png"),
-                ),
-                backgroundColor: Theme.of(context).primaryColor,
-                label: "Request",
+      ),
+      bottomNavigationBar: Obx(
+        () => BottomNavigationBar(
+          currentIndex: Get.find<StartupGlobalController>().currentIndex.value,
+          onTap: (index) {
+            Get.find<StartupGlobalController>().currentIndex.value = index;
+          },
+          items: [
+            BottomNavigationBarItem(
+              icon: ImageIcon(
+                AssetImage("assets/investor_detail_icon.png"),
               ),
-              BottomNavigationBarItem(
-                icon: ImageIcon(
-                  AssetImage("assets/notification_icon.png"),
-                ),
-                backgroundColor: Theme.of(context).primaryColor,
-                label: "Notification",
+              backgroundColor: Theme.of(context).primaryColor,
+              label: 'Investors',
+            ),
+            BottomNavigationBarItem(
+              icon: ImageIcon(
+                AssetImage("assets/connection_request_icon.png"),
               ),
-              BottomNavigationBarItem(
-                icon: ImageIcon(
-                  AssetImage("assets/test_image.png"),
-                ),
-                backgroundColor: Theme.of(context).primaryColor,
-                label: "Profile",
+              backgroundColor: Theme.of(context).primaryColor,
+              label: "Request",
+            ),
+            BottomNavigationBarItem(
+              icon: ImageIcon(
+                AssetImage("assets/notification_icon.png"),
               ),
-            ],
-          ),
+              backgroundColor: Theme.of(context).primaryColor,
+              label: "Notification",
+            ),
+            BottomNavigationBarItem(
+              icon: ImageIcon(
+                AssetImage("assets/test_image.png"),
+              ),
+              backgroundColor: Theme.of(context).primaryColor,
+              label: "Profile",
+            ),
+          ],
         ),
       ),
     );
